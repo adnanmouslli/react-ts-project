a03e4f4489299a6c07cd83ad42d438f54309bf6a
Adnan Mouslli <yazanmouslli6@gmail.com>
2024-08-31 15:47:21 -0700

    image component

---
+++ src/components/Images.tsx
@@ -0,0 +1,18 @@
+interface IProps {
+
+    imageUrl : string ,
+    alt : string ,
+    className : string
+
+}
+
+
+const Images = ({imageUrl , alt , className} : IProps) => {
+  return (
+    
+    <img src={imageUrl} alt={alt}  className={className}/>
+    
+  )
+}
+
+export default Images ;
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,3 +1,5 @@
+
+import Images from "./Images";
 interface IProps {
 
 
@@ -8,7 +10,11 @@ const ProductCard = ({} : IProps) => {
   return (
     <div className="border rounded-md p-4 text-slate-200 flex flex-col">
       
-      <img src="https://images3.alphacoders.com/133/1335169.png" alt="product" />
+
+      <Images 
+        imageUrl="https://images3.alphacoders.com/133/1335169.png" 
+        alt="product" 
+        className="rounded-md mb-3"  />
 
       <h3 className="text-black">2022 car verary model h2kk</h3>
     
@@ -23,11 +29,16 @@ const ProductCard = ({} : IProps) => {
 
       <div className="flex items-center justify-between text-black">
         <span >$500,000</span>
-        <img className="w-10 h-10 rounded-full" src="https://images3.alphacoders.com/133/1335169.png" alt="product" />
+     
+        <Images 
+        imageUrl="https://images3.alphacoders.com/133/1335169.png" 
+        alt="product" 
+        className="w-10 h-10 rounded-full object-bottom"  />
+
       </div>
 
 
-      <div className="text-black flex justify-between space-x-2 mt-5">
+      <div className="text-white flex justify-between space-x-2 mt-5">
         <button className="bg-indigo-700 p-2 w-full rounded-md">EDIT</button>
         <button className="bg-red-700 p-2 w-full rounded-md">DELETE</button>
       </div>
